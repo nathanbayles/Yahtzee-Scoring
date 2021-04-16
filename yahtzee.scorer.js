050799ceeaabc18824b958135017aeed01356c32
Nathan Bayles <nathanbayles@filevine.com>
2021-04-16 21:12:12 +0000

    Interview 1

--- yahtzee.scorer.js
+++ yahtzee.scorer.js
@@ -14,6 +14,7 @@ function score(fiveDiceRoll, scoringRule) {
 // [1,1,1,1,1] = 5
 function scoreChance(fiveDiceRoll)
 {
+    return fiveDiceRoll.reduce((prev, cur) => cur + prev);
 }
 
 // if the fiveDiceRoll[] contains three (or more) of a kind, return sum of all dice in fiveDiceRoll[]
@@ -22,6 +23,18 @@ function scoreChance(fiveDiceRoll)
 // [1,2,3,4,5] = 0
 function scoreThreeOfAKind(fiveDiceRoll)
 {
+    let counter = [0,0,0,0,0,0,0];
+    var maxCount = 0;
+    for(let i = 0; i < fiveDiceRoll.length; i++) 
+    {
+        counter[0] += fiveDiceRoll[i];
+        counter[fiveDiceRoll[i]] += 1;
+        if (counter[fiveDiceRoll[i]] > maxCount)
+        {
+            maxCount = counter[fiveDiceRoll[i]];
+        }        
+    }
+    return maxCount >= 3 ? counter[0] : 0;
 }
 
 module.exports = score;
