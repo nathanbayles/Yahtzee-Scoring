d8850dfab1e5cc43a627516f0a9f48e0beee9708
Nathan Bayles <nathanbayles@filevine.com>
2021-08-19 14:38:05 +0000

    updated example

--- yahtzee.scorer.js
+++ yahtzee.scorer.js
@@ -14,27 +14,14 @@ function score(fiveDiceRoll, scoringRule) {
 // [1,1,1,1,1] = 5
 function scoreChance(fiveDiceRoll)
 {
-    return fiveDiceRoll.reduce((prev, cur) => cur + prev);
 }
 
 // if the fiveDiceRoll[] contains three (or more) of a kind, return sum of all dice in fiveDiceRoll[]
 // else return 0
 // [1,2,3,2,2] = 10
-// [1,2,3,4,5] = 0
+// [6,2,3,2,5] = 0
 function scoreThreeOfAKind(fiveDiceRoll)
 {
-    let counter = [0,0,0,0,0,0,0];
-    var maxCount = 0;
-    for(let i = 0; i < fiveDiceRoll.length; i++) 
-    {
-        counter[0] += fiveDiceRoll[i];
-        counter[fiveDiceRoll[i]] += 1;
-        if (counter[fiveDiceRoll[i]] > maxCount)
-        {
-            maxCount = counter[fiveDiceRoll[i]];
-        }        
-    }
-    return maxCount >= 3 ? counter[0] : 0;
 }
 
 module.exports = score;
